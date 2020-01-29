84e22193cec531ecb6510667bb82346335b6659c
joan cruz <joancruz95@gmail.com>
2020-01-29 01:09:59 +0000

    Add city retrieve and delete methods

---
+++ api/v1/views/cities.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+New view for City objects that handles default Restful API actions
+"""
+from flask import Flask, jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.city import City
+from models.state import State
+
+
+@app_views.route('/api/v1/states/<state_id>/cities', strict_slashes=False)
+def all_cities(state_id):
+    """ retrieve list of all City objects of State"""
+    all_cities = []
+    for city in storage.all('City').values():
+        if state_id == city.state_id:
+            all_cities.append(city.to_dict())
+    if not all_cities:
+        abort(404)
+    return jsonify(all_cities)
+
+
+@app_views.route('/api/v1/cities/<city_id>', strict_slashes=False)
+def retrieve_city(city_id):
+    """ retrieve a particular City """
+    try:
+        city = jsonify(storage.get('City', city_id).to_dict())
+        return city
+    except:
+        abort(404)
+
+@app_views.route('/api/v1/cities/<city_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_city(city_id):
+    """ delete a City """
+    city = storage.get('City', city_id)
+    if city:
+        city.delete()
+        storage.save()
+        return {}
+    abort(404)
+
+@app_views.route('/api/v1/states/<state_id>/cities', methods=['POST'],
+                 strict_slashes=False)
+def create_cities(state_id):
+    """ create a City """
+    city_name = request.get_json()
+    if not city_name:
+        abort(400, {'Not a JSON'})
+    elif 'name' not in city_name:
+        abort(400, {'Missing name'})
+    city_name['state_id'] = state_id
+    new_city = City(**city_name)
+    storage.new(new_city)
+    storage.save()
+    return new_city.to_dict(), 201
+
+@app_views.route('/api/v1/states/<state_id>', methods=['PUT'],
+                 strict_slashes=False)
+def update_state(state_id):
+    """ update a State """
+    update_attr = request.get_json()
+    if not update_attr:
+        abort(400, {'Not a JSON'})
+    my_state = storage.get('State', state_id)
+    if not my_state:
+        abort(404)
+    for key, value in update_attr.items():
+        setattr(my_state, key, value)
+    return my_state.to_dict()
+
